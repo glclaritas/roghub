56af2a4055db392dfa6f32a9b3bb88d9c7907e8c
Naiko <glclaritas@gmail.com>
2025-10-28 14:41:34 +0630

    cpucontrol.c: switch to open() from fopen()

--- src/cpucontrol.c
+++ src/cpucontrol.c
@@ -1,4 +1,6 @@
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -9,18 +11,21 @@
 #define SCALING_DRIVER_FILE "/sys/devices/system/cpu/cpu0/cpufreq/scaling_driver"
 
 unsigned int cpu_get_freq(const char *path) {
-    unsigned int freq;
-    FILE *fptr = fopen(path, "r");
-    if(!fptr) {
-        fprintf(stderr, "Can't read cpu frequency values.\n");
-        return 0;
+    unsigned int freq = 0;
+    char buffer[16];
+    ssize_t len;
+    int fd = open(path, O_RDONLY);
+    if (fd == -1) {
+        fprintf(stderr, "Error-1. Can't read CPU freq.\n");
     }
-    if (fscanf(fptr, "%d",&freq) != 1) {
-        fprintf(stderr,"Failed to read %s.\n",path);
-        fclose(fptr);
-        return 0;
+    len = read(fd, buffer, sizeof(buffer));
+    close(fd); 
+    if (len <= 0) {
+        fprintf(stderr, "Error-2. Can't read CPU freq. Invalid input.\n");
+    } else {
+        buffer[len - 1] = '\0';
+        freq = (unsigned int)strtol(buffer, NULL, 10);
     }
-    fclose(fptr);
     return freq;
 }
 
@@ -29,20 +34,26 @@ int cpu_set_freq(unsigned int khz) {
         return 0;
     }
     if (khz > cpu_get_freq(CPU_MAX_FILE) || khz < cpu_get_freq(CPU_MIN_FILE) || khz == 0){
-        fprintf(stderr,"Target cpu frequency is not a valid value. %ul\n", khz);
+        fprintf(stderr,"Target cpu frequency is not a valid value. %u\n", khz);
         return 0;
     }
     char path[128];
     int ncpu = sysconf(_SC_NPROCESSORS_ONLN);
     for (int i = 0; i < ncpu; i++){
         snprintf(path, sizeof(path), CPU_FREQ_PATH "/cpu%d/cpufreq/scaling_max_freq",i);
-        FILE *fptr = fopen(path, "w");
-        if (fptr == NULL){
-            fprintf(stderr,"Can't set cpu frequencies. Permission denied!\n");
+        int fd = open(path, O_WRONLY | O_CLOEXEC);
+        if (fd == -1) {
+            fprintf(stderr,"Can't set cpu frequency values.\n");
             return 0;
         }
-        fprintf(fptr,"%u",khz);
-        fclose(fptr);
+        char buffer[16];
+        ssize_t len = snprintf(buffer, sizeof(buffer), "%u", khz);
+        if (write(fd, buffer, len) <= 0 ) {
+            fprintf(stderr,"Can't set cpu frequency values. Invalid input.\n");
+            close(fd);
+            return 0;
+        }
+        close(fd);
     }
     return 1;
 }
@@ -52,30 +63,40 @@ int cpu_set_turbo(int val) {
         fprintf(stderr, "Invalid boost mode value. %d\n",val);
         return 0;
     }
-    char governor[32];
-    char boost_path[128];
-    FILE *fptr = fopen(SCALING_DRIVER_FILE, "r");
-    if (!fptr) {
-        fprintf(stderr, "Can't read cpu boost state.\n");
+
+    char governor[32], boost_path[128];
+    int fd;
+    ssize_t len;
+
+    fd = open(SCALING_DRIVER_FILE, O_RDONLY);
+    if (fd == -1) {
+        fprintf(stderr, "Can't read scaling driver.\n");
         return 0;
     }
-    if (fscanf(fptr, "%s", governor) != 1) {
-        printf("Can't read %s.Exiting\n",SCALING_DRIVER_FILE);
-        fclose(fptr);
+    len = read(fd, governor, sizeof(governor));
+    close(fd);
+    if (len <= 0) {
+        fprintf(stderr, "Can't read scaling driver. Invalid input.\n");
         return 0;
     }
-    fclose(fptr);
+    if (governor[len-1] == '\n') governor[len-1] = '\0';
+    
     if (strcmp(governor, "intel_pstate") == 0){
         snprintf(boost_path, sizeof(boost_path), "/sys/devices/system/cpu/intel_pstate/no_turbo");
-        val = (val + 1) % 2;            // flip intel pstate takes !true input
+        val = !val;            // flip intel pstate takes !true input
     }else snprintf(boost_path, sizeof(boost_path), "/sys/devices/system/cpu/cpufreq/boost");
     
-    fptr = fopen(boost_path, "w");
-    if (!fptr) {
+    fd = open(boost_path, O_WRONLY | O_CLOEXEC);
+    if (fd == -1) {
         fprintf(stderr, "Can't change cpu boost state.\n");
         return 0;
     }
-    fprintf(fptr,"%d",val);
-    fclose(fptr);
+    char c = '0' + val;
+    len = write(fd, &c, 1);
+    close(fd);
+    if (len != 1) {
+        fprintf(stderr, "Can't change cpu boost state. Invalid input.\n");
+        return 0;
+    }
     return 1;
 }
