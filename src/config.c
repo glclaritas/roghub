7588a45668ab0d1a16762ccf337127e36f1486e6
Naiko <glclaritas@gmail.com>
2026-07-10 22:08:46 +0630

    better config reading
    can now support comments and key value parsing is better

--- src/config.c
+++ src/config.c
@@ -1,11 +1,16 @@
-#include <bits/posix2_lim.h>
 #include <limits.h>
-#include <linux/limits.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 #include "config.h"
+
+#define ASCII_HASH          0x23    // #
+#define ASCII_SEMICOLON     0x3B    // ;
+#define ASCII_NUL           0x00    // \0
+#define ASCII_LF            0x0A    // \n
+#define ASCII_DELIM         0x3D    // =
+#define ASCII_SPACE         0x20    // whitespace
 
 static char cpath[PATH_MAX];
 static config_t config;
@@ -15,50 +20,71 @@ config_t* cfg_update(void){
     char *home = getenv("HOME");
 
     if (xdg) {
-        snprintf(cpath,sizeof(cpath),"%s/roghub/config",xdg); 
+        snprintf(cpath,sizeof(cpath),"%s/roghub/config.ini",xdg); 
     } else if (home) {
-        snprintf(cpath,sizeof(cpath),"%s/.config/roghub/config",home); 
+        snprintf(cpath,sizeof(cpath),"%s/.config/roghub/config.ini",home); 
     } else {
-        fprintf(stderr, "Error: can't detemine config path.\n");
+        fprintf(stderr, "Error: can't determine config path.\n");
         return 0;
     }
-
-    FILE *cfile;
-    cfile = fopen(cpath, "r");
-    if (cfile == NULL){
+    
+    FILE *file = fopen(cpath, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: can't open file: %s.\n", cpath);
         return 0;
     }
+    
+    int current_line = 0;
+    int read_value_count = 0;
+    char *delim = NULL, *key = NULL, *val = NULL, *lastchar = NULL, *comment = NULL;
+    const char *comments = ";#";
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    while((nread = getline(&line, &len, file)) != -1) {
+        ++current_line;
+        if (line[0] == ASCII_SEMICOLON || line[0] == ASCII_HASH || line[0] == ASCII_SPACE || line[0] == ASCII_LF || line[0] == ASCII_DELIM) continue;
 
-    char line[LINE_MAX];
-    int read_value_count=0;
-    while (fgets(line, sizeof(line),cfile)){
-        char *fsptr;
+        line[nread-1] = ASCII_NUL;
+        lastchar = &line[nread-2];
 
-        fsptr = strchr(line, '\n');
-        if (!fsptr) {
-            fprintf(stderr,"Invalid config file!\n");
-            fclose(cfile);
-            return 0;
+        delim = strchr(line, ASCII_DELIM);
+        if (delim == NULL) {
+            delim = lastchar =  NULL;
+            continue;
         }
-        if (fsptr) *fsptr = '\0';
 
-        fsptr = strchr(line, '=');
-
-        if (!fsptr){
-            fprintf(stderr,"Invalid config file!\n");
-            fclose(cfile);
-            return 0;
+        comment = strpbrk(line, comments);
+        if (comment && *(comment-1) == ASCII_SPACE) {
+            lastchar = comment-1; 
         }
 
-        if (fsptr > line && *(fsptr -1) == ' ') {
-            *(fsptr-1) = '\0';
+        key = line;
+        val = delim+1;
+
+        // trim spaces around =
+        while(*(delim-1) == ASCII_SPACE) {
+            delim--;
         }
-        *fsptr = '\0';
-        char *key = line;
-        char *val = fsptr+1;
-        
-        while (*key == ' ') key++;
-        while (*val == ' ') val++;
+        *delim = ASCII_NUL;
+        while(*val == ASCII_SPACE) val++;
+
+        // trim leading spaces from keys
+        while(*key == ASCII_SPACE) {
+            key++;
+        }
+
+        // trim trailing spaces from values
+        while(*lastchar == ASCII_SPACE) {
+            *lastchar = ASCII_NUL;
+            lastchar--;
+        }
+
+        // empty key
+        if (key[0] == ASCII_NUL) {
+            delim = key = val = lastchar = NULL;
+            continue;
+        }
 
         if (strcmp(key, "name") == 0) {
             snprintf(config.name, sizeof(config.name), "%s", val);
@@ -76,8 +102,14 @@ config_t* cfg_update(void){
             config.turbo = atoi(val);
             read_value_count++;
         }
+
+        printf("Read config '%s':'%s'\n", key, val);
+
+        // reset
+        delim = key = val = lastchar = NULL;
     }
-    fclose(cfile);
+
+    fclose(file);
     if (read_value_count == 4) {
         return &config;
     } else return NULL;
